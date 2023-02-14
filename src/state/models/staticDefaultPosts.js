6e9762490eb1b8c88f49179212e332a92a0a1d9f
Pashkovska, Oksana <Oksana.Pashkovska@adesso.de>
2023-02-14 16:06:11 +0100

    fixed anonymous export in staticDefaultPosts

--- src/state/models/staticDefaultPosts.js
+++ src/state/models/staticDefaultPosts.js
@@ -1,14 +1,15 @@
-export default [
-    {
-        title: "Post 1",
-        author: "Author1",
-        content: "Content of post 1",
-        comments: [{ author: "Oksana", content: "Hello" }],
-    },
-    {
-        title: "Post 2",
-        author: "Author12",
-        content: "Content of post 2",
-        comments: [],
-    },
+const staticDefaultPosts = [
+	{
+		title: "Post 1",
+		author: "Author1",
+		content: "Content of post 1",
+		comments: [{ author: "Oksana", content: "Hello" }],
+	},
+	{
+		title: "Post 2",
+		author: "Author12",
+		content: "Content of post 2",
+		comments: [],
+	},
 ];
+export default staticDefaultPosts;
