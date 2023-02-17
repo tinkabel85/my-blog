b4fc06dbb471212682e4a358a42e7363a80e0fd4
Pashkovska, Oksana <Oksana.Pashkovska@adesso.de>
2023-02-17 10:00:50 +0100

    added 404 page

--- src/components/containers/Routes/Routes.js
+++ src/components/containers/Routes/Routes.js
@@ -2,14 +2,16 @@ import React from "react";
 import { Route, Routes as RouterRoutes } from "react-router-dom";
 import BlogPostDetailPage from "../../pages/BlogPostDetail/BlogPostDetailPage";
 import BlogPostListPage from "../../pages/BlogPostList/BlogPostListPage";
+import Err404Page from "../../pages/Err404/Err404Page";
 import LoginPage from "../../pages/Login/LoginPage";
 
 function Routes() {
 	return (
 		<RouterRoutes>
-			<Route path="/" element= {<BlogPostListPage />} />
-			<Route path="/post/:id" element={< BlogPostDetailPage/>} />
+			<Route path="/" element={<BlogPostListPage />} />
+			<Route path="/post/:id" element={<BlogPostDetailPage />} />
 			<Route path="/login" element={<LoginPage />} />
+			<Route path="*" element={<Err404Page />} />
 		</RouterRoutes>
 	);
 }
